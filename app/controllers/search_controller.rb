adec594856122688a43bdceb5c60245ab77d85dd
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-27 11:58:07 -0600

    ATP - Pagination complete

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def index
     @stores = Store.find_nearby_stores(params[:search]).paginate(:page => params[:page], :per_page => 10)
+    # binding.pry
     @total = Store.total(params[:search])
   end
 end
