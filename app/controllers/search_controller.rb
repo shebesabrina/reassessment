aa4bff1e78f31297fc07af95e34d0f6c87f7f880
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-27 11:48:39 -0600

    ATP for first feature store with refactoring. Working on pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @stores = Store.find_nearby_stores(params[:search])
+    @stores = Store.find_nearby_stores(params[:search]).paginate(:page => params[:page], :per_page => 10)
     @total = Store.total(params[:search])
   end
 end
